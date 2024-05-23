ebb3834aa808c557b170489129c478353534d3d9
carlos andrés castilla garcía <wowzeros2@gmail.com>
2024-05-22 19:15:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,71 @@
-# Lista global para almacenar los pacientes
-list_pacientes = []
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Any
+
+app = FastAPI()
 
 
-# Función para registrar un nuevo paciente
-def registro_paciente():
-    print("*****************************")
-    print("Registro de paciente")
-    print("*****************************")
+@app.get("/")
+def read_root():
+    return {"hello": "world"}
 
-    # Solicitar la cantidad de pacientes a registrar
-    numeros_paciente = int(input("Digite la cantidad de pacientes: "))
+libros = []
+usuarios = []
+libros_usuarios = []
 
-    # Iterar sobre el número de pacientes ingresados
-    for i in range(numeros_paciente):
-        nombre = input("Digite nombre del paciente: ")
-        edad = input("Digite la edad del paciente: ")
-        numero_social = input("Digite el número de seguridad social del paciente:")
+class Usuario(BaseModel):
+    nombre: str
+    id: int
 
-        # Crear un diccionario para almacenar la información del paciente
-        paciente = dict(
-            nombre=nombre,
-            edad=edad,
-            numero_social=numero_social,
-        )
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    estado: int = 0
+    id: int = None
 
-        # Agregar el paciente a la lista de pacientes
-        list_pacientes.append(paciente)
-
-        # Mostrar la información del paciente registrado
-        mostrar_paciente(paciente)
-
-
-# Función para mostrar la información de un paciente
-def mostrar_paciente(paciente):
-    print("*****************************")
-    print("INFORMACIÓN DEL PACIENTE")
-    print("*****************************")
-    print("Datos:")
-    print("Nombre: ", paciente["nombre"])
-    print("Edad: ", paciente["edad"])
-    print("Número social: ", paciente["numero_social"])
-    if "temperatura" in paciente:
-        print("Temperatura: ", paciente["temperatura"])
+    def guardar(self):
+        if self.id is None:
+            self.id = len(libros) + 1
+            libros.append(self)
+        else:
+            for libro in libros:
+                if libro.id == self.id:
+                    libro.autor = self.autor
+                    libro.titulo= self.titulo
+                    libro.estado = self.estado
+        return self
 
 
-# Función para agregar la temperatura de un paciente
-def agregar_temperatura_al_paciente():
-    print("*****************************")
-    print("Agregar temperatura de paciente")
-    print("*****************************")
-    n_social = input("Digite el número social del paciente:")
+    @classmethod
+    def consultar(cls, libro_id):
+        for libro in libros:
+            if libro.id == libro_id:
+                return libro
+        return None
 
-    # Buscar al paciente por su número de seguridad social
-    for paciente in list_pacientes:
-        if paciente["numero_social"] == n_social:
-            paciente["temperatura"] = input("Digite la temperatura del paciente:")
-            break
+    @classmethod
+    def consultar_todos(cls):
+        return libros
 
 
-# Función para mostrar un resumen de un paciente
-def mostrar_resumen():
-    print("*****************************")
-    print("Mostrar resumen del paciente")
-    print("*****************************")
-    n_social = input("Digite el número social del paciente:")
 
-    # Buscar al paciente por su número de seguridad social
-    for paciente in list_pacientes:
-        if paciente["numero_social"] == n_social:
-            mostrar_paciente(paciente)
-            break
+class LibroUsuario():
+    usuario_id : int
+    libro_id : int
+    tiempo_prestamo : int
 
+    def __init__(self):
+        pass
 
-# Función para mostrar todos los pacientes registrados
-def mostrar_todos_pacientes():
-    print("*****************************")
-    print("Mostrar todos los pacientes")
-    print("*****************************")
-    for paciente in list_pacientes:
-        mostrar_paciente(paciente)
+@app.post("/libro")
+async def crear_libro(libro: Libro):
+    return libro.guardar()
 
 
-# Función para el menú de opciones
-def menu(opt):
-    # Diccionario que mapea las opciones a las funciones correspondientes
-    tabla_menu = {
-        "1": registro_paciente,
-        "2": agregar_temperatura_al_paciente,
-        "3": mostrar_resumen,
-        "4": mostrar_todos_pacientes
-    }
-    # Llamar a la función correspondiente según la opción ingresada
-    return tabla_menu[opt]()
+@app.get("/libros")
+async def consultar_libro():
+    return Libro.consultar_todos()
 
-
-if __name__ == '__main__':
-    seguir = "s"
-    while seguir.lower() == "s":
-        print("*****************************")
-        print("Clinica DevZeros S.A.S")
-        print("*****************************")
-
-        print("Opciones de menu:")
-        print("1> Agregar y mostrar Registro paciente")
-        print("2> Agregar temperatura al paciente en grados celsius")
-        print("3> Mostrar Resumen")
-        print("4> Mostrar todos los pacientes")
-        opt = input("Digite una opción: ")
-        menu(opt)
-        seguir = input("Ejecucicón terminada, desea seguir? S/N")
+@app.get("/libro/{libro_id}")
+async def consultar(libro_id : int):
+    return Libro.consultar(libro_id)
